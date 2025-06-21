d02c6c649011671ae6817dcdd850f8e62cf2d7f1
KhangPhuHo <hophukhang10c14@gmail.com>
2025-06-22 01:04:48 +0700

    node2

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -3,7 +3,7 @@ const cloudinary = require("cloudinary").v2;
 console.log(">> CLOUDINARY CONFIG CHECK <<");
 console.log("CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("API_KEY:", process.env.CLOUDINARY_API_KEY);
-console.log("API_SECRET:", process.env.CLOUDINARY_SECRET_KEY);
+console.log("SECRET_KEY:", process.env.CLOUDINARY_SECRET_KEY);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -13,4 +13,5 @@ cloudinary.config({
 });
 
 module.exports = cloudinary;
+
 
