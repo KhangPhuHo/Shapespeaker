9b3f75ea23c84a950535a46d048b9b18e641eca8
KhangPhuHo <hophukhang10c14@gmail.com>
2025-06-22 00:10:54 +0700

    update

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'https://shapespeaker-7g6744zik-grr20091s-projects.vercel.app'
+    'https://shapespeaker-xv283xipv-grr20091s-projects.vercel.app'
   ],
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
